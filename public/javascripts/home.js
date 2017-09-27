02b09422cbbf9b3300f6dcca9d138aec60b59da3
nahidebnahasan <nahidebnahasan@gmail.com>
2017-09-27 10:23:14 +0600

    fix issue of opening chat-box

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -44,6 +44,10 @@ $(document).ready(function () {
         var new_chat_bubble = $('.frame');
         new_chat_bubble.attr('id', 'chat-bubble-' + $(this).attr('data_user_id'));
         new_chat_bubble.find('.chat-header').html($(this).attr('data_user_name'));
+        new_chat_bubble.find('input').attr('data_receiver_id', $(this).attr('data_user_id'));
+        new_chat_bubble.find('input').attr('data_receiver_name', $(this).attr('data_user_name'));
+        new_chat_bubble.find('input').attr('data_sender_id', user_id);
+        new_chat_bubble.find('input').attr('data_sender_name', user_name);
         new_chat_bubble.css('display', 'block');
 //        var new_chat_bubble = '<div class="col-sm-3 col-sm-offset-4 frame" id="chat-bubble-' + $(this).attr('data_user_id') + '">' +
 //            '<div class="col-sm-12 chat-header">' + $(this).attr('data_user_name') +'</div>' +
@@ -86,7 +90,84 @@ $(document).ready(function () {
 //        console.log(html);
         $('#chat-users').html(html);
     });
+    
+    $(document).on("keyup", ".mytext", function(e){
+        if (e.which == 13){
+            var text = $(this).val();
+            var sender_id = $(this).attr('data_sender_id');
+            var sender_name = $(this).attr('data_sender_name');
+            var receiver_id = $(this).attr('data_receiver_id');
+            var receiver_name = $(this).attr('data_receiver_name');
+            if (text !== ""){
+                insertChat("me", text, $('chat-bubble-' + receiver_id));
+                socket.emit('send_text', { sender: { id: sender_id, name: sender_name }, receiver: { id: receiver_id, name: receiver_name }, message: text });
+                $(this).val('');
+            }
+        }
+    });
+    
+    socket.on('receive_text', function(data){
+        if(data['receiver_id'] == user_id){
+            var target = $('#chat-bubble-' + data['receiver']['id']);
+            if(target != undefind && target.is(":visible")){
+               insert_text('you', data['text'], target);
+            }else{
+               var new_chat_bubble = '<div class="col-sm-3 col-sm-offset-4 frame" id="chat-bubble-' + data['sender']['id'] + '">' +
+                '<div class="col-sm-12 chat-header">' + data['sender']['name'] +'</div>' +
+                '<div class="chat-body">' +
+                '<ul>' +
+                '<div>' +
+                '<div class="msj-rta macro" style="margin:auto">' +
+                '<div class="text text-r" style="background:whitesmoke !important">' +
+                '<input class="mytext" placeholder="Type a text" data_sender_id="' + data['sender']['id'] + '" data_sender_name="' + data['sender']['name'] + '" data_receiver_id="' + data['reciver']['id'] + '" data_receiver_name="' + data['receiver']['name'] + '">' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</ul>' +
+                '</div>' +
+                '</div>';
+                $('.chat-bubbles').html(new_chat_bubble);
+            }
+        }
+    });
+                
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+                
+    function insertChat(who, text, target){
+        var control = "";
+        var date = formatAMPM(new Date());
 
+        if (who == "me"){
+            control = '<li style="width:100%">' +
+                            '<div class="msj macro">' +
+                            '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                                '<div class="text text-l">' +
+                                    '<p>'+ text +'</p>' +
+                                    '<p><small>'+date+'</small></p>' +
+                                '</div>' +
+                            '</div>' +
+                        '</li>';                    
+        }else{
+            control = '<li style="width:100%;">' +
+                            '<div class="msj-rta macro">' +
+                                '<div class="text text-r">' +
+                                    '<p>'+text+'</p>' +
+                                    '<p><small>'+date+'</small></p>' +
+                                '</div>' +
+                            '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +                                
+                      '</li>';
+        }
+        target.find("ul").append(control);
+    }
     socket.on('update_available_rooms', function (data) {
 //        console.log('AVAILABLE ROOMS: ' + data);
         available_rooms = data;
@@ -139,7 +220,7 @@ $(document).ready(function () {
         }
         $('#title-room-name').text(data['room_name']);
         chat_display.append(data['user_name'] + ' has joined the room ' + data['room_name'] + '\n');
-    })
+    });
 });
 
 function init_socket(user_id, user_name){
